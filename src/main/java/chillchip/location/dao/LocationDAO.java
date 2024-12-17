45c9d9cd96a8187fd0e96cde0364ac38b5450d6e
Hsiu-chiLiu <l224762938@gmail.com>
2024-12-17 23:37:12 +0800

    景點Servlet全部完成（postman測試完畢）

--- src/main/java/chillchip/location/dao/LocationDAO.java
+++ src/main/java/chillchip/location/dao/LocationDAO.java
@@ -11,9 +11,8 @@ public interface LocationDAO {
 	public void update(LocationVO locationVO);
 	public void delete(Integer locationid);
 	public List<LocationVO> getAll();
-	public List<LocationVO> getAll(int currentPage);
 	public List<Map<String, Object>> getAllPro();
 	public LocationVO getById(Integer locationid);
-	public List<Map<String, Object>> getByLocationName(String location_name);
+	public List<Map<String, Object>> getLocationByName(String location_name);
 
 }
